76075492c1fd01a05b89dfb109d9e3f927cadbf1
Shedlia.Freeman <shedliaf@basecampcodingacademy.org>
2017-11-16 10:14:20 -0600

    Mapped over my data structure using thumbnails

--- Data.js
+++ Data.js
@@ -1,22 +1,50 @@
 var Phone_Data = [
     {
-        Apple: {
+        manufacturer: 'Apple',
+        details: {
             ProductName: 'Iphone X',
             Price: 999.99,
             description:
                 "Phone X. You've never seen anything like it. It's never seen anything like you.",
-            Picture: '',
+            Picture:
+                'https://drop.ndtv.com/TECH/product_database/images/913201720152AM_635_iphone_x.jpeg',
             Seller: 'Boe'
         }
     },
     {
-        Apple: {
+        manufacturer: 'Apple',
+        details: {
             ProductName: 'Iphone 7',
             Price: 499.99,
             description:
                 "Phone 7. You've never seen anything like it. It's never seen anything like you.",
-            Picture: '',
+            Picture:
+                'https://store.storeimages.cdn-apple.com/4974/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone7/silver/iphone7-silver-select-2016?wid=1200&hei=630&fmt=jpeg&qlt=95&op_sharpen=0&resMode=bicub&op_usm=0.5,0.5,0,0&iccEmbed=0&layer=comp&.v=1472430219571',
             Seller: 'Billy'
+        }
+    },
+    {
+        manufacturer: 'Samsung',
+        details: {
+            ProductName: 'Galaxy Not8',
+            Price: 949.99,
+            description:
+                "The Galaxy Note8 and AT&T unleash your potential to create and enjoy. Do bigger things with Samsung's latest Infinity Display, smart S Pen, the world's first Dual Camera with Optical Image Stabilization on both lenses, and the power of the AT&T network.",
+            Picture:
+                'https://s7d2.scene7.com/is/image/SamsungUS/Note8-L-S-Pen-Midnight-Black?$product-details-jpg$',
+            Seller: 'Jane'
+        }
+    },
+    {
+        manufacturer: 'Samsung',
+        details: {
+            ProductName: 'Galaxy S8 Active',
+            Price: 849.99,
+            description:
+                "The Galaxy Note8 and AT&T unleash your potential to create and enjoy. Do bigger things with Samsung's latest Infinity Display, smart S Pen, the world's first Dual Camera with Optical Image Stabilization on both lenses, and the power of the AT&T network.",
+            Picture:
+                'https://www.att.com/catalog/en/skus/images/samsung-galaxy%20s8-orchid%20gray-450x350.png',
+            Seller: 'John'
         }
     }
 ];
--- index.js
+++ index.js
@@ -2,15 +2,25 @@ function getInventory(data) {
     return data
         .map(function(phone) {
             return [
-                '<p> Product Name: ' + phone.Apple.ProductName + '</p>',
-                '<p> Price: ' + phone.Apple.Price + '</p>',
-                '<p> Seller: ' + phone.Apple.Seller + '</p>',
-                '<p> Description: ' + phone.Apple.description + '</p>'
+                "<div class='col-sm-6 col-md-4'>",
+                "<div class='thumbnail'>",
+                "<img src='" + phone.details.Picture + "' alt='...'>",
+                "<div class='caption'>",
+                '<h3>' +
+                    phone.manufacturer +
+                    ' ' +
+                    phone.details.ProductName +
+                    '</h3>',
+                '<p>' + phone.details.description + '</p>',
+                "<p><a href='#' class='btn btn-primary' role='button'>Button</a> <a href='#' class='btn btn-default' role='button'>Button</a></p>",
+                '</div>',
+                '</div>',
+                '</div>'
             ].join('');
         })
         .join('');
 }
-
+function makeRow(data) {}
 function main() {
     $('#phones').html(getInventory(Phone_Data));
 }
